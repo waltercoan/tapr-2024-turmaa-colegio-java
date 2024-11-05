6e004491a00d78f7001da006c6cb00953a29903b
Walter Silvestre Coan <walter.coan@gmail.com>
2024-11-05 00:55:27 +0000

    implementacao do service

---
+++ microservcolegio/src/main/java/br/univille/microservcolegio/secretaria/service/AlunoService.java
@@ -0,0 +1,10 @@
+package br.univille.microservcolegio.secretaria.service;
+
+import java.util.List;
+
+import br.univille.microservcolegio.secretaria.entity.Aluno;
+
+public interface AlunoService {
+    List<Aluno> getAll();
+    Aluno save(Aluno aluno);
+}
