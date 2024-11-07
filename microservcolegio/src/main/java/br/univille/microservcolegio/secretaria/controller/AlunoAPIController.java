2bcd366f141b3bf12c82cbfcc3dff9f054575ed1
Walter Silvestre Coan <walter.coan@gmail.com>
2024-11-07 00:59:06 +0000

    get alunos

---
+++ microservcolegio/src/main/java/br/univille/microservcolegio/secretaria/controller/AlunoAPIController.java
@@ -0,0 +1,28 @@
+package br.univille.microservcolegio.secretaria.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.univille.microservcolegio.secretaria.entity.Aluno;
+import br.univille.microservcolegio.secretaria.service.AlunoService;
+
+@RestController
+@RequestMapping("/api/v1/alunos")
+public class AlunoAPIController {
+    
+    @Autowired
+    private AlunoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> get(){
+        var listaAlunos = service.getAll();
+
+        return new ResponseEntity<List<Aluno>>(listaAlunos, HttpStatus.OK);
+    }
+
+}
